c1fbf0177e8bc128cf8afff4e2ab52fcf64ad05c
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-08-09 16:41:54 +0000

    updated init.py with Mongo

--- skincare_package/__init__.py
+++ skincare_package/__init__.py
@@ -1,14 +1,10 @@
-import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-if os.path.exists("env.py"):
-    import env  # noqa
+from flask_pymongo import PyMongo
 
-
-app = Flask(__name__)
+app = Flask(__name__)  # initialize app as package
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
-db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
-from skincare_package import routes  # noqa
+from skincare_package import routes 
